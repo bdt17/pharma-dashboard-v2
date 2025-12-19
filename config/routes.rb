f2f9e7db843d750ae249a39b93fafd223681437e
bdt17 <brett.thomas29.97@gmail.com>
2025-12-18 20:57:51 -0700

    add api/vehicles route for dashboard trucks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+get '/api/vehicles', to: 'vehicles#index'
   root "dashboard#index"
 
   # DRIVER ROUTES - COMPLETE
