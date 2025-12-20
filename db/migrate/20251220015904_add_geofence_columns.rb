4d3ca7261aa508aeca0a6c34f5ee1fe08c2832f2
bdt17 <brett.thomas29.97@gmail.com>
2025-12-19 19:02:45 -0700

    remove duplicate geofence migrations

--- db/migrate/20251220015904_add_geofence_columns.rb
+++
@@ -1,8 +0,0 @@
-class AddGeofenceColumns < ActiveRecord::Migration[8.1]
-  def change
-    add_column :geofences, :name, :string
-    add_column :geofences, :latitude, :float
-    add_column :geofences, :longitude, :float
-    add_column :geofences, :radius, :float
-  end
-end
