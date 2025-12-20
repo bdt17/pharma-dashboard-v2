2c8f57f642242712094c07e8ea95bb16402392d2
bdt17 <brett.thomas29.97@gmail.com>
2025-12-19 18:37:04 -0700

    LIVE GPS MOVEMENT

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -55,3 +55,13 @@ def update_gps
   end
   render json: Vehicle.all
 end
+
+def update_gps
+  Vehicle.all.each do |v|
+    v.update_columns(
+      latitude: [[v.latitude + rand(-0.001..0.001), 33.3, 36.3].min, 33.3].max,
+      longitude: [[v.longitude + rand(-0.001..0.001), -115.5, -111.5].min, -115.5].max
+    )
+  end
+  render json: Vehicle.all
+end
