49c65b24c81a45dde346a54a0aa7fbc6ebc46a42
bdt17 <brett.thomas29.97@gmail.com>
2025-12-19 18:39:23 -0700

    FIX routes + clean duplicates

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,62 +6,30 @@ class VehiclesController < ApplicationController
     respond_with @vehicles
   end
 
-  def update_gps
-    Vehicle.all.each do |v|
-      v.update_columns(
-        latitude: [v.latitude + rand(-0.001..0.001), 33.3, 36.3].clamp(33.3, 36.3),
-        longitude: [v.longitude + rand(-0.001..0.001), -115.5, -111.5].clamp(-115.5, -111.5)
-      )
-    end
     respond_with Vehicle.all
   end
 end
 
+    render json: Vehicle.all
+  end
+
+    render json: Vehicle.all
+  end
+  render json: Vehicle.all
+end
+
+  render json: Vehicle.all
+end
+
+  render json: Vehicle.all
+end
+
   def update_gps
     Vehicle.all.each do |v|
       v.update_columns(
-        latitude: [v.latitude + rand(-0.001..0.001), 33.3, 36.3].clamp(33.3, 36.3),
-        longitude: [v.longitude + rand(-0.001..0.001), -115.5, -111.5].clamp(-115.5, -111.5)
+        latitude: [[v.latitude + rand(-0.001..0.001), 33.3, 36.3].min, 33.3].max,
+        longitude: [[v.longitude + rand(-0.001..0.001), -115.5, -111.5].min, -115.5].max
       )
     end
     render json: Vehicle.all
   end
-
-  def update_gps
-    Vehicle.all.each do |v|
-      v.update_columns(
-        latitude: [v.latitude + rand(-0.001..0.001), 33.3, 36.3].clamp(33.3, 36.3),
-        longitude: [v.longitude + rand(-0.001..0.001), -115.5, -111.5].clamp(-115.5, -111.5)
-      )
-    end
-    render json: Vehicle.all
-  end
-def update_gps
-  Vehicle.all.each do |v|
-    v.update_columns(
-      latitude: [v.latitude + rand(-0.001..0.001), 33.3, 36.3].clamp(33.3, 36.3),
-      longitude: [v.longitude + rand(-0.001..0.001), -115.5, -111.5].clamp(-115.5, -111.5)
-    )
-  end
-  render json: Vehicle.all
-end
-
-def update_gps
-  Vehicle.all.each do |v|
-    v.update_columns(
-      latitude: [[v.latitude + rand(-0.001..0.001), 33.3, 36.3].min, 33.3].max,
-      longitude: [[v.longitude + rand(-0.001..0.001), -115.5, -111.5].min, -115.5].max
-    )
-  end
-  render json: Vehicle.all
-end
-
-def update_gps
-  Vehicle.all.each do |v|
-    v.update_columns(
-      latitude: [[v.latitude + rand(-0.001..0.001), 33.3, 36.3].min, 33.3].max,
-      longitude: [[v.longitude + rand(-0.001..0.001), -115.5, -111.5].min, -115.5].max
-    )
-  end
-  render json: Vehicle.all
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "admin#index"
   resources :vehicles
-end
   get '/update_gps', to: 'vehicles#update_gps'
+end
