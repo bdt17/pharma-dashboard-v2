fbc4abcc2295f190ff68d0ee99b37666890012fb
bdt17 <brett.thomas29.97@gmail.com>
2025-12-19 18:25:50 -0700

    FIX: routes get INSIDE draw block

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root "admin#index"
-  
-  get "/vehicles", to: "vehicles#index"
-  get "/vehicles/update_gps", to: "vehicles#update_gps"
-  
   resources :vehicles
+  get '/update_gps', to: 'vehicles#update_gps'
 end
-get '/update_gps', to: 'vehicles#update_gps'
