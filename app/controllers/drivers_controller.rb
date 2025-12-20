4c90c8a0139d5019d75405b859b8fcff69578444
bdt17 <brett.thomas29.97@gmail.com>
2025-12-19 20:28:54 -0700

    Fix 500: drivers table + controller + form

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,10 +1,20 @@
 class DriversController < ApplicationController
   def new
+    @driver = Driver.new
     render layout: 'dashboard'
   end
   
   def create
-    # Driver signup logic
-    redirect_to dashboard_path, notice: 'Driver added!'
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to dashboard_path, notice: 'Driver added!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def driver_params
+    params.require(:driver).permit(:name, :phone)
   end
 end
