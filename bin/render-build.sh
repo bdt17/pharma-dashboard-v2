60596a28da3465d241e96fa38e0d0657be5b24d6
bdt17 <brett.thomas29.97@gmail.com>
2025-12-27 01:10:07 -0700

    Fix Render DB connection + migrations

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
 bundle config set --local without 'development test'
 bundle install
+
+# Create/provision DB if needed, then migrate
+bin/rails db:create
 bin/rails db:migrate
