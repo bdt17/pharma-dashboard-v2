6de388f2764bbe7f0eda0f716a5e4a65c028f274
bdt17 <brett.thomas29.97@gmail.com>
2026-01-05 20:24:58 -0700

    Add dashboard directory + controller + view

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,10 @@
+class DashboardController < ApplicationController
+  def index
+    @audit_entries = defined?(AuditLog) ? AuditLog.last(50) : []
+    @recent_shipments = defined?(Shipment) ? Shipment.limit(10) : []
+  end
+
+  def shipments; end
+  def audit_trail; end
+  def subscription_required; end
+end
