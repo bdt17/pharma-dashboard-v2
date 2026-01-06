3c46c999bff4c11d2e5d84092508772d6a299f73
bdt17 <brett.thomas29.97@gmail.com>
2026-01-05 21:31:25 -0700

    Fix dashboard: proper HTML + pharma styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,7 @@ class DashboardController < ApplicationController
     @audit_entries = defined?(AuditLog) ? AuditLog.last(50) : []
     @recent_shipments = defined?(Shipment) ? Shipment.limit(10) : []
   end
-
   def shipments; end
-  def audit_trail; end
+  def audit_trail; end  
   def subscription_required; end
 end
